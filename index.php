9fb8b70c5621fd5b286a9e118a10d51e445576d7
Anderson Gordillo <mauri201111@gmail.com>
2024-12-05 11:29:07 -0500

    Update Index.php 05122024

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API WITH PHP USING MYSQL & SQLSERVER</title>
+</head>
+<body>
+    HERE SHARE INFORMATION ABOUT API-REST
+</body>
+</html>
