d023c446ed1bcae4b47ba2b0edc3d3894c53bf4b
Anderson Gordillo <mauri201111@gmail.com>
2024-12-09 09:07:03 -0500

    Fin Curso API-REST
    He entendido los 4 request, GET, POST. DELETE Y PUT USANDO POSTMAN
    
    Conectandose al backend creado por PHP y este a la vez esta conectado a la base de datos MySQL usando el servidor WAMP y adicional a ello un gestor de bases de datos DBEAVER para gestionar diferentes tipos de bases de datos MySQL, SQLSERVER, ORACLE, etc..... Usando Visual studio como editor de codigo

--- index.php
+++ index.php
@@ -12,6 +12,46 @@
                 //echo 'Este es el metodo GET';
             }
             break;
+        case 'POST':
+            $datos = json_decode(file_get_contents('php://input'));
+            if($datos != NULL){
+                if(Cliente::insert($datos->nombre, $datos->apellido1, $datos->apellido2, $datos->fechaN, $datos->genero)){
+                    http_response_code(200);
+                }
+                else {
+                    http_response_code(400);
+                }
+            }
+            else {
+                http_response_code(405);
+            }
+            break;
+        case 'PUT':
+            $datos = json_decode(file_get_contents('php://input'));
+            if($datos != NULL){
+                if(Cliente::update($datos->id, $datos->nombre, $datos->ap, $datos->am, $datos->fn, $datos->genero)){
+                    http_response_code(200);
+                }
+                else {
+                    http_response_code(400);
+                }
+            }
+            else {
+                http_response_code(405);
+            }
+            break;
+        case 'DELETE':
+            if(isset($_GET['id'])){
+                if(Cliente::delete($_GET['id'])){
+                    http_response_code(200);
+                }
+                else {
+                    http_response_code(400);
+                }
+            }
+            else {
+                http_response_code(405);
+            }
 
         default:
             break;
